fdf873a43dc931be071dda00192ef11e70f456f4
areos <areos.cc@gmail.com>
2022-07-28 13:10:01 +0930

    test for github actions

--- test/general.test.js
+++ test/general.test.js
@@ -4,8 +4,12 @@ const testFn = (a, b) => {
 };
 
 describe("Basic Test", () => {
-    it("check 1st", () => {
+    it("check 1", () => {
         const result = testFn(1, 2);
         expect(result).toBe(3);
+    });
+    it("check 2", () => {
+        const result = testFn(2, 2);
+        expect(result).toBe(4);
     });
 });
